d9bbfa870270e177f84840d237f185d2ead8d746
mattcs2401 <mattcs2401@gmail.com>
2017-07-18 14:27:22 +1000

    Updates for shopping list model.

---
+++ app/src/main/java/mcssoft/com/todolist/model/ShoppingList.java
@@ -0,0 +1,32 @@
+package mcssoft.com.todolist.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utility class that models a shopping list.
+ */
+public class ShoppingList {
+
+    public ShoppingList() {
+        shoppingList = new ArrayList<ShoppingListItem>();
+    }
+
+    public ShoppingList getShoppingList() {
+        return (ShoppingList) shoppingList;
+    }
+
+    public ShoppingListItem getShoppingListItem(int position) {
+        if(shoppingList.size() > 0) {
+            return shoppingList.get(position);
+        } else {
+            return null;
+        }
+    }
+
+    public void putShoppingListItem(ShoppingListItem item) {
+        shoppingList.add(item);
+    }
+
+    private List<ShoppingListItem> shoppingList;
+}
---
+++ app/src/main/java/mcssoft/com/todolist/model/ShoppingListItem.java
@@ -0,0 +1,58 @@
+package mcssoft.com.todolist.model;
+
+/**
+ * Utility class to model a shopping list item.
+ * Note: The SLIST_ITEM table is just a linking table between SLIST and REF_ITEM. The values in this
+ *       class were originally sourced from REF_ITEM.
+ */
+public class ShoppingListItem {
+
+    public ShoppingListItem(boolean isHeader) {
+        this.isHeader = isHeader;
+    }
+
+    public ShoppingListItem(String code, String description, String value) {
+        this.code = code;
+        this.description = description;
+        this.value = value;
+        this.isHeader = false;
+    }
+
+    public boolean isHeader() {
+        return isHeader;
+    }
+
+    public void setHeader(boolean header) {
+        isHeader = header;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    private boolean isHeader;     // if set then essentially an empty item (used for a header row).
+    private String code;          //
+    private String description;   //
+    private String value;         //
+
+}
