940bf73c19b2da79ae7ef77aea469577b3e85937
mattcs2401 <mattcs2401@gmail.com>
2017-07-25 14:52:05 +1000

    ShoppingItemsListItem model.

--- app/src/main/java/mcssoft/com/todolist/model/items/ShoppingItemsListItem.java
+++ app/src/main/java/mcssoft/com/todolist/model/items/ShoppingItemsListItem.java
@@ -1,6 +1,53 @@
 package mcssoft.com.todolist.model.items;
 
+/**
+ * Class that basically models a REF_ITEM.
+ */
 public class ShoppingItemsListItem {
+
+    public ShoppingItemsListItem(String refCode, String refDesc, String refValue, String refSelect) {
+        this.refCode = refCode;
+        this.refDesc = refDesc;
+        this.refValue = refValue;
+        this.refSelect = refSelect;
+    }
+
+    public String getRefCode() {
+        return refCode;
+    }
+
+    public void setRefCode(String refCode) {
+        this.refCode = refCode;
+    }
+
+    public String getRefDesc() {
+        return refDesc;
+    }
+
+    public void setRefDesc(String refDesc) {
+        this.refDesc = refDesc;
+    }
+
+    public String getRefValue() {
+        return refValue;
+    }
+
+    public void setRefValue(String refValue) {
+        this.refValue = refValue;
+    }
+
+    public String isRefSelect() {
+        return refSelect;
+    }
+
+    public void setRefSelect(String refSelect) {
+        this.refSelect = refSelect;
+    }
+
+    private String refCode;
+    private String refDesc;
+    private String refValue;
+    private String refSelect;
 
 }
 //    public static final String REF_ITEM_CODE    = "REF_CODE";    // e.g. GENRL, FANDV etc.
