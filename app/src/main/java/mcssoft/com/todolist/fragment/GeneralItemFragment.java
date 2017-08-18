5e95aa32cd33d744475c88e3abad66f0b0628e9b
mattcs2401 <mattcs2401@gmail.com>
2017-08-18 14:37:30 +1000

    Initial cut of general item fragment and editable fields.

--- app/src/main/java/mcssoft/com/todolist/fragment/GeneralItemFragment.java
+++ app/src/main/java/mcssoft/com/todolist/fragment/GeneralItemFragment.java
@@ -1,15 +1,23 @@
 package mcssoft.com.todolist.fragment;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import mcssoft.com.todolist.R;
+import mcssoft.com.todolist.utility.Resources;
 
-public class GeneralItemFragment extends Fragment {
+public class GeneralItemFragment extends Fragment
+        implements View.OnClickListener, View.OnKeyListener, View.OnTouchListener { //}, View.OnFocusChangeListener { //} {
 
     //<editor-fold defaultstate="collapsed" desc="Region: Lifecycle">
     @Override
@@ -21,11 +29,59 @@ public class GeneralItemFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
+        nameLabelEdit = (EditText) rootView.findViewById(R.id.id_et_nameLabel);
+        nameLabelEdit.setOnClickListener(this);
+        nameLabelEdit.setOnKeyListener(this);
+//        nameLabelEdit.setOnTouchListener(this);
+//        nameLabelEdit.setCursorVisible(false);
+//        nameLabelEdit.setFocusable(true);
         args = getArguments();
     }
     //</editor-fold>
 
+
+    @Override
+    public void onClick(View view) {
+//        if(view instanceof FloatingActionButton) {
+        String bp = "";
+//        }
+        if(view instanceof EditText) {
+
+            nameLabelEdit.setCursorVisible(true);
+//            showSoftKeyboard(view);
+        }
+
+    }
+
+    public void showSoftKeyboard(View view) {
+        if (view.requestFocus()) {
+            InputMethodManager imm = (InputMethodManager)
+                    getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+        }
+    }
+
+    @Override
+    public boolean onKey(View view, int keyCode, KeyEvent event) {
+        if( keyCode == KeyEvent.KEYCODE_ENTER ) {
+//            if( event.getAction() == KeyEvent.ACTION_DOWN ) {
+                nameLabelEdit.setCursorVisible(false);
+                InputMethodManager imm = (InputMethodManager)
+                        getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+//            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+//        nameLabelEdit.setCursorVisible(true);
+        return false;
+    }
+
+    private EditText nameLabelEdit;
     private Bundle args;
     private View rootView;
 
